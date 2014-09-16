33ce143dd226c78a41748212bfa7b97d738780f7
Asier <Campus@192.168.30.203>
2014-09-16 08:27:33 +0200

    Merge origin/master

--- Main.java/src/main/MainJava.java
+++ Main.java/src/main/MainJava.java
@@ -11,11 +11,16 @@ package main;
  */
 public class MainJava {
 
+    public static void SuperSuma(){
+        System.out.println((2+2));
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        SuperSuma();
     }
     
 }
