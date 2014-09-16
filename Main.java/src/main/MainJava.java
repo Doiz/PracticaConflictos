9e0d81061bcb43b7a58daade0bdb839c6b200b1b
Davizhe <>
2014-09-16 10:30:03 +0200

    Añadido metodo despedida

--- Main.java/src/main/MainJava.java
+++ Main.java/src/main/MainJava.java
@@ -14,8 +14,14 @@ public class MainJava {
     /**
      * @param args the command line arguments
      */
+    public static void despedida(){
+        System.out.println("Adios!");
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        despedida();
+        
     }
     
 }
